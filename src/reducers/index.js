6b2d62d15ca115926db75864728f74603136e328
Carla Romero <carlagromero@gmail.com>
2021-05-06 16:02:00 -0300

    chore: configure redux

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import commentsReducer from 'reducers/comments';
+
+const reducer = () => (
+  combineReducers({
+    comments: commentsReducer,
+  })
+)
+
+export default reducer;
