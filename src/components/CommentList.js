ced6dcac756565957d4b4e8212ae47aba8e1b8d3
Carla Romero <carlagromero@gmail.com>
2021-05-06 18:00:08 -0300

    chore: show comments list

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const CommentList = () => {
+  const comments = useSelector(state => state.comments);
+
   return (
-    <div>Comment List</div>
+    <div>
+      <h4>Comment List</h4>
+      <ul>
+        {comments.map(comment => (
+          <li key={comment}>
+            {comment}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
