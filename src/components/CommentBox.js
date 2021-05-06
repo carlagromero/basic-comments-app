3ace1592ec37fb3865fd17c40e856f47ad61cb71
Carla Romero <carlagromero@gmail.com>
2021-05-06 15:42:22 -0300

    chore: update CommentBox component

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,8 +1,26 @@
 import React from 'react';
 
 const CommentBox = () => {
+  const [comment, setComment] = React.useState('');
+
+  const handleChangeTextArea = e => {
+    setComment(e.target.value);
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    setComment('');
+  }
+
   return (
-    <div>Comment Box</div>
+    <form onSubmit={handleSubmit}>
+      <h4>Add a comment</h4>
+      <textarea value={comment} onChange={handleChangeTextArea} />
+      <div>
+        <button>Submmit comment</button>
+      </div>
+    </form>
   );
 };
 
